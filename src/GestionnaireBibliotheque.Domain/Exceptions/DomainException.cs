281727ac786b43718cbeaea3dd4550168a4b3de8
Franck Ntangke <franck.ntangke@gmail.com>
2026-07-16 12:34:09 +0200

    ajout des exceptions

--- src/GestionnaireBibliotheque.Domain/Exceptions/DomainException.cs
+++ src/GestionnaireBibliotheque.Domain/Exceptions/DomainException.cs
@@ -1,4 +1,3 @@
 namespace GestionnaireBibliotheque.Domain.Exceptions;
 
-/// <summary>Exception de base signalant la violation d'une règle métier du domaine.</summary>
 public class DomainException(string message) : Exception(message);
